7299bc4fdff9be4117a3f6ca792a9876c334ad5b
Harshal <Hpatil879@gmail.com>
2019-11-04 15:43:44 +0530

    Navbar initial stage

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Register from "./components/Register";
 import ResetPassword from "./components/ResetPassword";
 import Login from "./components/Login";
+import NavbarDefault from "./components/NavbarDefault"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route exact path="/register" component={Register} />
         <Route exact path="/resetpassword" component={ResetPassword} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/navdemo" component={NavbarDefault} />
+
       </div>
     </Router>
   );
