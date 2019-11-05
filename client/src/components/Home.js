62900e02aaa91d5b744256727b07c4d979ce27cb
piyush kumar <kinngpiyush7@gmail.com>
2019-11-05 11:53:08 +0530

    home page at client side

---
+++ client/src/components/Home.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { allNews, newsById } from "../actions/Home";
+import { connect } from "react-redux";
+import "../styles/Home.css";
+import image from "./fakelogo.png";
+
+export class Home extends Component {
+  componentDidMount() {
+    //this function calls the get News function which is defined in the actions which will retrive all the data from the news table.
+    this.props.allNews();
+    this.props.newsById(1);
+  }
+
+  render() {
+    console.log(this.props.singleNews);
+    return (
+      <body>
+        <div id="homecontainer">
+          {/* <Navbar> */}
+          <div id="homeleftsidecontainer">
+            <h1>News</h1>
+            <div>
+              <div id="recent-news"></div>
+              {/* recent news */}
+              {this.props.news.map(news => (
+                <div className="div-newspage">
+                  <div id="news-list">
+                    <p
+                      id="recent-news-title"
+                      onClick={() => this.props.newsById(news.new_id)}
+                    >
+                      {news.headline}>
+                    </p>
+                    <p id="newsdivider">
+                      <hr />
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div id="homemiddlecontainer">
+            <div>
+              <h3>{this.props.singleNews.headline}</h3>
+              <img
+                id="image"
+                src={
+                  "data:image/jpeg;base64," + this.props.singleNews.news_image
+                }
+              />
+              <p>{this.props.singleNews.description}</p>
+            </div>
+          </div>
+          <div id="homerightsidecontainer"></div>
+        </div>
+      </body>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  news: state.homeReducer.news,
+  singleNews: state.homeReducer.singleNews
+});
+export default connect(
+  mapStateToProps,
+  {
+    allNews,
+    newsById
+  }
+)(Home);
