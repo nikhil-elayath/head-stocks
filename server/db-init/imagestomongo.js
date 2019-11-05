25f2c3982903f557aff2fd1f03f5ea878b851d29
Bhavana Gupta <Bhavana>
2019-11-05 12:14:47 +0530

    Script for image Upload

--- server/db-init/imagestomongo.js
+++ server/db-init/imagestomongo.js
@@ -2,27 +2,35 @@ var mongodb = require("mongodb");
 var mongoClient = mongodb.MongoClient;
 var fs = require("fs");
 
-var url = "mongodb://localhost:27017/headStocks";
+// Connection URL
+const url = "mongodb://localhost:27017";
 
+// Database Name
+const dbName = "headstock";
+var imageAsBase64 = fs.readFileSync(
+  process.cwd() + "\\newsimage\\1.jpg",
+  "base64"
+);
+console.log(imageAsBase64);
 mongoClient.connect(url, function(err, db) {
   if (err) {
     console.log("Sorry unable to connect to MongoDB Error:", err);
   } else {
-    var bucket = new mongodb.GridFSBucket(db, {
-      chunkSizeBytes: 1024,
-      bucketName: "images"
-    });
-
-    fs.createReadStream(
-      "C:UserskinngDesktopcapstone2headstocksserverdb-init\newsimage\1.jpg"
-    )
-      .pipe(bucket.openUploadStream("1.jpg"))
-      .on("error", function(error) {
-        console.log("Error:-", error);
-      })
-      .on("finish", function() {
-        console.log("File Inserted!!");
-        process.exit(0);
-      });
+    console.log("Connected successfully to server");
+    const db = db.db(dbName);
+    try {
+      var collection = db.collection("news");
+      collection.update(
+        { _id: 100 },
+        {
+          $set: {
+            news_image: imageAsBase64
+          }
+        }
+      );
+      console.log("Image Inserted");
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
