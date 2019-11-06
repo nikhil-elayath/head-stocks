07d6701a1c3e5722aa1532d908fdcadd80d1f57e
Nikhil Elayath <nikhil.elayath13@gmail.com>
2019-11-06 14:39:24 +0530

    Created CompanyDetailFinancils and CompanyDetailAnalysis

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Register from './components/Register'
-import ResetPassword from './components/ResetPassword'
-import Login from './components/Login'
-import home from './components/Home'
-import NavbarDefault from './components/NavbarDefault'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/Register";
+import ResetPassword from "./components/ResetPassword";
+import Login from "./components/Login";
+import home from "./components/Home";
+import NavbarDefault from "./components/NavbarDefault";
 // nikhil
-import CompanyDetail from './components/CompanyDetail'
-import CompanyDetailSecondaryNavbar from './components/Common/CompanyDetailSecondaryNavbar'
+import CompanyDetail from "./components/CompanyDetail";
+import CompanyDetailSecondaryNavbar from "./components/Common/CompanyDetailSecondaryNavbar";
 // aditya
-import StocksLanding from './components/StocksLanding'
+import StocksLanding from "./components/StocksLanding";
+import { CompanyDetailFinancial } from "./components/CompanyDetailFinancial";
+import CompanyDetailAnalysis from "./components/CompanyDetailAnalysis";
 
-function App () {
+function App() {
   return (
     <Router>
       <div>
         <NavbarDefault />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/resetpassword' component={ResetPassword} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/' component={home} />
-        <Route exact path='/companydetail' component={CompanyDetail} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/resetpassword" component={ResetPassword} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={home} />
+        <Route exact path="/companydetail" component={CompanyDetail} />
         {/* aditya */}
-        <Route exact path='/stocks' component={StocksLanding} />
+        <Route exact path="/stocks" component={StocksLanding} />
         <Route
           exact
-          path='/secondarynavbar'
+          path="/secondarynavbar"
           component={CompanyDetailSecondaryNavbar}
         />
+        <Route exact path="/financial" component={CompanyDetailFinancial} />
+        <Route exact path="/analysis" component={CompanyDetailAnalysis} />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
