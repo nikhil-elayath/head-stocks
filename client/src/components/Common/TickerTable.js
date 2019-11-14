c1242cb937a4b71e981f6a8e4e81726e2b5d33ba
Bhavana Gupta <Bhavana>
2019-11-14 09:27:12 +0530

    Reusable Table

---
+++ client/src/components/Common/TickerTable.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+export class TickerTable extends Component {
+  render() {
+    console.log(this.props.isIndex);
+    console.log(this.props.tableHeaders);
+    return (
+      <div className="table__container">
+        <table className="ticker__table">
+          <thead className="ticker__tableHeader">
+            <tr>
+              {this.props.tableHeaders.map((data, index) => (
+                <th>{data}</th>
+              ))}
+            </tr>
+          </thead>
+          {console.log("tabledata", typeof this.props.tableData)}
+          {this.props.tableData.map((current, index) => (
+            <tr>
+              {this.props.isIndex == true ? (
+                <td
+                  onClick={() => {
+                    this.props.exchangeIndexVis(current.ticker_name);
+                  }}
+                >
+                  {current.ticker_name ? current.ticker_name : null}
+                </td>
+              ) : (
+                <Link to={{ pathname: "/stocks/" + current.ticker_id }}>
+                  <td
+                  // onClick={() => {
+                  //    this.props.history.push("/stocks/" + current.ticker_id);
+                  // }}
+                  >
+                    {current.ticker_name ? current.ticker_name : null}
+                  </td>
+                </Link>
+              )}
+              {current.tickerValues
+                ? Object.keys(current.tickerValues).map((keyName, index) => (
+                    <>
+                      {current.tickerValues[keyName].substring(0, 1) == "-" ? (
+                        <td style={{ color: "var(--red-color)" }}>
+                          {current.tickerValues[keyName]}
+                        </td>
+                      ) : current.tickerValues[keyName].substring(0, 1) ==
+                        "+" ? (
+                        <td style={{ color: "var(--green-color)" }}>
+                          {current.tickerValues[keyName]}
+                        </td>
+                      ) : (
+                        <td>{current.tickerValues[keyName]}</td>
+                      )}
+                    </>
+                  ))
+                : null}
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({});
+export default connect(mapStateToProps, {})(TickerTable);
