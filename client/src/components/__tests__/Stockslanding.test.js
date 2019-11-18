b04d11454d2001673bcdaf5ededfcdeb05169259
adityashetty <adishetty1234@gmail.com>
2019-11-18 13:00:30 +0530

    updated stocks landing page

--- client/src/components/__tests__/Stockslanding.test.js
+++ client/src/components/__tests__/Stockslanding.test.js
@@ -6,10 +6,44 @@ import {
   getIndustries,
   getGainersLosers
 } from "../../actions/Stocks";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 
-var stocks = [],
-  gainersLosers = [],
+const stocks = [
+    {
+      ticker_id: 169,
+      ticker_name: "ANTM",
+      ticker_logo: "image",
+      MarketCap: 21223.9902,
+      "Share Price": 51.35
+    }
+  ],
+  gainersLosers = [
+    {
+      isIndex: false,
+      gainers: [
+        {
+          tickerValues: {
+            change_percent: "+30.37",
+            "Market Cap": "68.00M",
+            "Share Price": "4.38"
+          },
+          ticker_name: "HPJ",
+          ticker_id: 1030
+        }
+      ],
+      losers: [
+        {
+          tickerValues: {
+            change_percent: "-33.33",
+            "Market Cap": "13.00M",
+            "Share Price": "0.03"
+          },
+          ticker_name: "TGLO",
+          ticker_id: 2007
+        }
+      ]
+    }
+  ],
   sectors = [],
   industries = [];
 
@@ -26,11 +60,54 @@ const wrapper = shallow(
   />
 );
 
-describe("Testing stocks Component", () => {
+describe("Testing Stocks Component", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
   it("should mount the component", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  it("Table should have button with text Gainers", () => {
+    expect(wrapper.find("#button_stocks_gainers").text()).toBe("Gainers");
+  });
+  it("Table should have button with text Losers", () => {
+    expect(wrapper.find("#button_stocks_losers").text()).toBe("Losers");
+  });
+  it("should simulate button click on Gainers tab ", () => {
+    wrapper.find("#button_stocks_gainers").simulate("click");
+    expect(wrapper.state().gainersClick).toBe(true);
+  });
+  it("should simulate button click on Gainers tab ", () => {
+    wrapper.find("#button_stocks_losers").simulate("click");
+    expect(wrapper.state().gainersClick).toBe(false);
+  });
+  // it("should simulate change on selector dropdown ", () => {
+  //   wrapper.find("#stocks_dropdown_sectors").simulate("change");
+  // });
+  it("should simulate click on stocks card ", () => {
+    wrapper.find("stocks_grid_details").simulate("click");
+  });
+  it("Closed price should be shown in each stocks card", () => {
+    expect(wrapper.find("#stocks_ticker").text()).toBe("ANTM");
+  });
+  it("should check for presense of OnSelectSector function", () => {
+    let OnSelectSector = jest.spyOn(wrapper.instance(), "OnSelectSector");
+    expect(OnSelectSector).toBeTruthy();
+  });
+  it("should check for presense of OnSelectSector function", () => {
+    let OnSelectSector = jest.spyOn(wrapper.instance(), "OnSelectSector");
+    expect(OnSelectSector).toBeTruthy();
+  });
+  it("should check for presense of OnSelectIndustry function", () => {
+    let OnSelectIndustry = jest.spyOn(wrapper.instance(), "OnSelectIndustry");
+    expect(OnSelectIndustry).toBeTruthy();
+  });
+  it("should check for presense of displayCompanies function", () => {
+    let displayCompanies = jest.spyOn(wrapper.instance(), "displayCompanies");
+    expect(displayCompanies).toBeTruthy();
+  });
+  it("should check for presense of loadMoreItems function", () => {
+    let loadMoreItems = jest.spyOn(wrapper.instance(), "loadMoreItems");
+    expect(loadMoreItems).toBeTruthy();
   });
 });
