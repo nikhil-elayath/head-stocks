b6c9f1e1efb338fbd03ee90c6fa4e9debcd52a9f
Nikhil Elayath <nikhil.elayath13@gmail.com>
2019-11-19 10:17:14 +0530

    changed id of index close company detail secondary navbar

--- client/src/components/CompanyDetailAnalysis.js
+++ client/src/components/CompanyDetailAnalysis.js
@@ -9,7 +9,7 @@ import {
   getmonteCarloCompany1,
   getmonteCarloCompany2,
   getAssetsCompany1,
-  getAssetsCompany2
+  getAssetsCompany2,
 } from "../actions/CompanyDetail";
 import { connect } from "react-redux";
 import "../styles/CompanyDetailAnalysis.css";
@@ -31,7 +31,7 @@ export class CompanyDetailAnalysis extends Component {
   // this action is to plot the graph
   OnSelectTicker = e => {
     this.setState({
-      ticker_name: e.target.value
+      ticker_name: e.target.value,
     });
     this.props.getGaugeCompany2(e.target.value);
     this.props.getAssetsCompany2(e.target.value);
@@ -43,27 +43,6 @@ export class CompanyDetailAnalysis extends Component {
       <div>
         {/* checking whether the data has been loaded into the reducer and if it is then getSimilarTabl which will have a parameter sector from the data loaded in the reducer  */}
         <CompanyDetailSecondaryNavbar />
-        {/* <div style={{ width: "1000px", margin: "auto" }}>
-          {this.props.similar_company["0"] ? (
-            <>
-              {console.log(this.props.similar_company["0"].similar_sector_data)}
-              <Table
-                tableHeaders={[
-                  "Ticker",
-                  "Dividends",
-                  "Market Cap",
-                  "Net Profit",
-                  "P/E ratio",
-                  "Share Price",
-                  "ROCE%"
-                ]}
-                tableData={this.props.similar_company["0"]}
-              />
-            </>
-          ) : (
-            <p>Loading </p>
-          )}
-        </div> */}
         {/* DROPDOWN FOR SIMILAR COMPANIES  */}
         <div id="company-analysis-similar-cimpany-dropdown">
           <select
@@ -114,7 +93,7 @@ export class CompanyDetailAnalysis extends Component {
                   width: "650px",
                   height: "500px",
                   outline: "none",
-                  border: "none"
+                  border: "none",
                 }}
               />
             </div>
@@ -126,7 +105,7 @@ export class CompanyDetailAnalysis extends Component {
                 width: "650px",
                 height: "500px",
                 outline: "none",
-                border: "none"
+                border: "none",
               }}
             />
           </div>
@@ -146,7 +125,7 @@ const mapStateToProps = state => ({
   maonteCarlo1: state.CompanyDetailReducer.maonteCarlo1,
   maonteCarlo2: state.CompanyDetailReducer.drop_down_data,
   assets1: state.CompanyDetailReducer.assets1,
-  assets2: state.CompanyDetailReducer.assets2
+  assets2: state.CompanyDetailReducer.assets2,
 });
 export default connect(mapStateToProps, {
   getCompanyDetailById,
@@ -157,5 +136,5 @@ export default connect(mapStateToProps, {
   getmonteCarloCompany1,
   getmonteCarloCompany2,
   getAssetsCompany1,
-  getAssetsCompany2
+  getAssetsCompany2,
 })(CompanyDetailAnalysis);
