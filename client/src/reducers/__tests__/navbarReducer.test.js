2902cd054292f498b235321920c4ff87b5b633b0
Harshal <Hpatil879@gmail.com>
2019-11-19 10:25:24 +0530

    Comments added financials

--- client/src/reducers/__tests__/navbarReducer.test.js
+++ client/src/reducers/__tests__/navbarReducer.test.js
@@ -2,6 +2,7 @@ import navbarReducer from "../navbarReducer";
 import { SEARCH_CONTENT } from "../../actions/Types";
 
 describe("Testing Navbar Reducer", () => {
+  // Test for search results array storing the results of search
   it("should return a state object with search results array equal to the payload in the action when the action type is SEARCHCONTENT (when the returned state is initial state)", () => {
     const action = {
       type: SEARCH_CONTENT
@@ -10,6 +11,7 @@ describe("Testing Navbar Reducer", () => {
     expect(returnedState).toEqual({ results: action.payload });
   });
 
+  // Test for the empty array of search results if some other action called
   it("should return the initial state object when the action type is not mentioned or doesn't concern the reducer (when the returned state is initial state", () => {
     let action = {
       results: []
