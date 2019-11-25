a23883d621757fe64453e5a6643b869bd46669fc
Nikhil Elayath <nikhil.elayath13@gmail.com>
2019-11-25 16:35:48 +0530

    mongo query changes in advance filter

--- server/routes/api/ScreenerSearch.js
+++ server/routes/api/ScreenerSearch.js
@@ -18,6 +18,8 @@ router.post("/screener", async (req, res, next) => {
   const market_cap_value2 = req.body.market_cap_value2;
   const price_to_equity_ratio1 = req.body.price_to_equity_ratio1;
   const price_to_equity_ratio2 = req.body.price_to_equity_ratio2;
+  const share_price1 = req.body.share_price1;
+  const share_price2 = req.body.share_price2;
 
   const search_result = [];
   try {
@@ -27,6 +29,7 @@ router.post("/screener", async (req, res, next) => {
           $and: [{ sector: req.body.sector }, { industry: req.body.industry }],
         },
       },
+
       { $unwind: "$ticker_dates" },
       {
         $project: {
@@ -35,21 +38,7 @@ router.post("/screener", async (req, res, next) => {
           ticker_name: 1,
         },
       },
-      // {
-      //   $group: {
-      //     // _id is used for string the year and month of the data of each quarter distinctly.
-      //     _id: {
-      //       // Year of the quarter is extracted and stored
-      //       year: { $year: "$ticker_dates.date" },
-      //       // Month of the quarter is extracted and stored
-      //       month: { $month: "$ticker_dates.date" },
-      //       day: { $dayOfMonth: "$ticker_dates.date" },
-      //     },
-      //     // all the data of quarters is stores in the key named date_values
-      //     // $push is used to store the dates in this key
-      //     date_values: { $push: "$ticker_dates" },
-      //   },
-      // },
+
       {
         $sort: {
           // _id key has the values stored of year and month
@@ -61,71 +50,81 @@ router.post("/screener", async (req, res, next) => {
           "ticker_dates.date": -1,
         },
       },
-      { $arrayElemAt: ["$ticker_dates", 0] },
+      // {
+      //   $group: {
+      //     name: { $addToSet: "$ticker_name" },
+      //     date_values: { $push: "$ticker_dates" },
+      //   },
+      // },
+
+      // { $arrayElemAt: ["$ticker_dates", 0] },
       {
         $match: {
           "ticker_dates.Market Capitalisation": {
             $gt: +market_cap_value1,
             $lt: +market_cap_value2,
           },
+          "ticker_dates.Share Price": {
+            $gt: +share_price1,
+            $lt: +share_price2,
+          },
         },
       },
-      // { $unwind: "$date_values" },
     ]);
 
     console.log("printing result", result);
-    result.forEach(function(elem) {
-      let compare = {};
-      console.log("within for each");
-      for (let i of result) {
-        console.log("i", i.ticker_dates);
-        console.log("iiis", i.ticker_dates["Market Capitalisation"]);
-        // traversing in the dates within each quarter
-        for (dates of i.ticker_dates) {
-          // checks for a particular key to be present on a single date
-          // Some Common Keys can be used here Eg:Total Assets, Net Profit,etc
-          if (dates.hasOwnProperty("Market Capitalisation")) {
-            // console.log(dates);
-            // if 'Key' found then that date data is pushed into
-            // the empty array(datesCF) declared on top
-            console.log("market cap found"); // console.log(y)
-          }
-        }
-      }
+    // result.forEach(function(elem) {
+    //   let compare = {};
+    //   console.log("within for each");
+    //   for (let i of result) {
+    //     // console.log("i", i.ticker_dates);
+    //     console.log("iiis", i.ticker_dates["Market Capitalisation"]);
+    //     // traversing in the dates within each quarter
+    //     for (dates of i.ticker_dates) {
+    //       // checks for a particular key to be present on a single date
+    //       // Some Common Keys can be used here Eg:Total Assets, Net Profit,etc
+    //       if (dates.hasOwnProperty("Market Capitalisation")) {
+    //         // console.log(dates);
+    //         // if 'Key' found then that date data is pushed into
+    //         // the empty array(datesCF) declared on top
+    //         console.log("market cap found"); // console.log(y)
+    //       }
+    //     }
+    //   }
 
-      ticker_dates = elem._doc.ticker_dates;
-      console.log("ticker_dates", ticker_dates);
+    //   ticker_dates = elem._doc.ticker_dates;
+    //   console.log("ticker_dates", ticker_dates);
 
-      last_date = ticker_dates.slice(-1)[0];
-      // console.log(last_date);
+    //   last_date = ticker_dates.slice(-1)[0];
+    //   // console.log(last_date);
 
-      var i = -1;
-      //fetching dividends
-      while (last_date["Dividends"] == undefined) {
-        last_date = ticker_dates.slice(i)[0];
+    //   var i = -1;
+    //   //fetching dividends
+    //   while (last_date["Dividends"] == undefined) {
+    //     last_date = ticker_dates.slice(i)[0];
 
-        i--;
-        // console.log("lastdate", last_date);
-      }
-      var dividend = last_date["Dividends"];
-      // console.log("dividend", dividend);
+    //     i--;
+    //     // console.log("lastdate", last_date);
+    //   }
+    //   var dividend = last_date["Dividends"];
+    //   // console.log("dividend", dividend);
 
-      compare["dividend"] = dividend.toString();
-      // console.log("printing compare", compare);
-      if (last_date["Market Capitalisation"] == undefined) {
-        Market_cap = "-";
-      } else {
-        Market_cap = last_date["Market Capitalisation"];
-      }
-      compare["marketcap"] = Market_cap.toString();
+    //   compare["dividend"] = dividend.toString();
+    //   // console.log("printing compare", compare);
+    //   if (last_date["Market Capitalisation"] == undefined) {
+    //     Market_cap = "-";
+    //   } else {
+    //     Market_cap = last_date["Market Capitalisation"];
+    //   }
+    //   compare["marketcap"] = Market_cap.toString();
 
-      search_result.push(compare);
-    });
+    //   search_result.push(compare);
+    // });
     console.log("printing search result", search_result);
     if (result.length == 0) {
       res.status(400).json({
         status: 400,
-        data: search_result,
+        data: result,
         message: "No result",
       });
     } else {
